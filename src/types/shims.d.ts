5252313eaaa967fc997605811dd1e2c4a37d4d74
asasinmode <skin9470@gmail.com>
2022-12-06 13:04:45 +0100

    route meta interface

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,5 +1,14 @@
+import 'vue-router'
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+    layout?: any
+  }
 }
